c21c2f56b9de83c8725effa3c585a97b7aca1469
nvdinh98 <86923066+nvdinh98@users.noreply.github.com>
2021-07-30 11:39:44 +0700

    update

--- code1.js
+++ code1.js
@@ -1,6 +1,7 @@
 let Product = new Array();
 let Price = new Array();
 let Description = new Array();
+let Picture = new Array();
 Product.push("Iphone 15 ");
 Product.push("Samsung j15");
 Product.push("Nokia E69");
@@ -10,6 +11,9 @@ Price.push("10.000.000");
 Description.push("Đẳng cấp")
 Description.push("Sang trọng")
 Description.push("Hàng siêu bền")
+Picture.push("../pictrue/iphone.png")
+Picture.push("../pictrue/nokia.png")
+Picture.push("../pictrue/sam.png")
 
 //=== show sản phẩm
 function showProduct() {
@@ -17,10 +21,11 @@ function showProduct() {
     textTable = "<table >";
     //====================Tạo tiêu đề bảng ===============================
     textTable += "<tr>";
-    textTable += "<th border-spacing: 5px>" + "Product" + "</th>";
-    textTable += "<th border-spacing: 5px>" + "Price" + "</th>";
-    textTable += "<th border-spacing: 5px>" + "Description" + "</th>";
-    textTable += "<th border-spacing: 5px>" + Product.length + "_" + "Product" + "</th>";
+    textTable += "<th border-spacing: 8px>" + "Product" + "</th>";
+    textTable += "<th border-spacing: 8px>" + "Price" + "</th>";
+    textTable += "<th border-spacing: 8px>" + "Description" + "</th>";
+    textTable += "<th border-spacing: 8px>" + "Picture" + "</th>";
+    textTable += "<th border-spacing: 8px>" + Product.length + "_" + "Product" + "</th>";
     textTable += "</tr>";
     //====================Tạo row(hàng) show Product ===============================
     for (let i = 0; i < Product.length; i++) {
@@ -28,6 +33,7 @@ function showProduct() {
         textTable += "<td>" + Product[i] + "</td>";
         textTable += "<td>" + Price[i] + "</td>";
         textTable += "<td>" + Description[i] + "</td>";
+        textTable += "<td>" + "<img src="+ Picture[i] +" height="+69+" width="+160+"/></td>";
         textTable += "<td>" + "<button onclick='checkProduct(this.value)' value=" + i + ">Edit</button>" + "<button onclick='deleteProduct(this.value)' value=" + i + ">Delete</button>" + "</td>";
         textTable += "</td>";
         textTable += "</tr>";
@@ -54,6 +60,7 @@ function checkProduct(id) {
     document.getElementById("editProduct").value = Product[id];
     document.getElementById("editDescription").value = Description[id]
     document.getElementById("editPrice").value = Price[id];
+    document.getElementById("editPicture").value = Picture[id];
     document.getElementById("btn-Edit").value = id;
 }
 
@@ -62,6 +69,7 @@ function editProduct(id) {
     Product[id] = document.getElementById("editProduct").value;
     Description[id] = document.getElementById("editDescription").value;
     Price[id] = document.getElementById("editPrice").value;
+    Picture[id] = document.getElementById("Picture").value;
     showProduct();
 }
 
@@ -70,8 +78,10 @@ function addProduct() {
     let productName = document.getElementById("addProduct").value;
     let productDescription = document.getElementById("addDescription").value;
     let productPrice = document.getElementById("addPrice").value;
+    let productPicture = document.getElementById("addPicture").value;
     Product.push(productName);
     Description.push(productDescription);
     Price.push(productPrice);
+    Picture.push(productPicture);
     showProduct();
 }
